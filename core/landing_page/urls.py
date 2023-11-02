6fc044b36cfff6d47bb05337fb61781ad9cd8b85
Sebastvin <Sebastvin@gmail.com>
2023-11-02 21:25:58 +0100

    Add simple views with examples

--- core/landing_page/urls.py
+++ core/landing_page/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-
+from landing_page.views import index, text_from_article, workshop_article, workshop_twitter
 
 urlpatterns = [
+    path("", index, name="home"),
+    path("article-text", text_from_article, name="text_article"),
+    path("workshop_article", workshop_article, name="text"),
+    path("workshop_twitter", workshop_twitter, name="twitter"),
 ]
