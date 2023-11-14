c11996aa76c8a91b63ec4b5d1288d398ef7acce6
Sebastvin <Sebastvin@gmail.com>
2023-11-14 23:49:47 +0100

    Change website layout

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,11 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
