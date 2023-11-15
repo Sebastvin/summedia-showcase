ef203a6e2ab37641a46553fb68eac6cf2891217a
Sebastvin <Sebastvin@gmail.com>
2023-11-15 23:13:29 +0100

    Fix staticfiles

--- core/settings.py
+++ core/settings.py
@@ -131,13 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
+
 
 
 # Default primary key field type
@@ -145,6 +142,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
