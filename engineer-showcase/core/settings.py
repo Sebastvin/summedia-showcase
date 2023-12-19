8d7b1850a2aeadc8b1fdb08fd135324900f3fdbc
Sebastvin <cherry@Cherrys-MacBook-Pro.local>
2023-12-19 21:05:54 +0100

    Black files

--- engineer-showcase/core/settings.py
+++ engineer-showcase/core/settings.py
@@ -132,11 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-
-
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
